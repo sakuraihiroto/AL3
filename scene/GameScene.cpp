ea7d7b33c1d84822f658cbf02152013931e33099
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-04-11 14:07:02 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,110 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+
+using namespace DirectX;
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	// 3Dモデルの生成
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	//サウンドデータの読み込み
+	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
+	//音楽再生
+	audio_->PlayWave(soundDataHandle_);
+	//音声再生
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+}
+
+void GameScene::Update() {
+	//スプライトの今の座標を取得
+	XMFLOAT2 position = sprite_->GetPosition();
+	//座標を{2,0}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+	//スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		//音楽停止
+		audio_->StopWave(voiceHandle_);
+	}
+	//デバイステキストの表示
+	debugText_->Print("kaizokuou ni oreha naru.", 50, 50, 1.0f);
+	//書式指定付き表示
+	debugText_->SetPos(50, 70);
+	debugText_->Printf("year:%d", 2001);
+	//変数の値をインクリメント
+	value_++;
+	//値を含めた文字列
+	std::string strDebug = std::string("Value:") + std::to_string(value_);
+	//デバッグテキストの表示
+	debugText_->Print(strDebug, 50, 50, 1.0f);
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+	sprite_->Draw();
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DebugText.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include <DirectXMath.h>
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+  public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+	/// <summary>
+	/// 初期化
+	/// </summary>
+
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+  private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	DebugText* debugText_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	// 3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+	//サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	//音楽再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//値を表示したい変数
+	int32_t value_ = 0;
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+};
